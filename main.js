223ecebd822ef6c2f7e3a2fc5e53ecd3131636af
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-10 13:26:00 +0100

    finished the logic, about to enter the debuging phase if there is any

--- main.js
+++ main.js
@@ -32,8 +32,72 @@ const playGame = () => {
         }
 
     }
-}
+};
 
 const getPlayerChoice = () =>{
     return prompt ("please enter rock, paper, or scissors.");
-}
+};
+
+const formatPlayerChoice = (playerChoice) => {
+    if (playerChoice || playerChoice === "") {
+        return playerChoice.trim().toLowerCase();
+    } else {
+        return false;
+    }
+};
+
+const decidedNotToPlay = () => {
+    alert("I guess you changed your mind. Maybe next time.");
+};
+
+const evaluatePlayerChioce = (playerChoice) => {
+    if (
+        playerChoice === "rock" ||
+        playerChoice === "paper" ||
+        playerChoice === "scissors" ||
+    ) {
+        return playerChoice;
+    } else {
+        return false;
+    }
+};
+
+const invalidChoice = () => {
+    alert("You didn't enter rock, paper or scissors");
+};
+
+const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    const rpsArray = ["rock", "paper", "scissors"];
+    return rpsArray[randomNumber];
+};
+
+const determineWiner = (player, computer) => {
+    const winner =
+        player === computer
+            ?"Tie game!"
+            : player === "rock" && computer === "paper"
+            ?`playerOne: ${player}\nComputer: ${computer}\nComputer Wins!`
+            :player === "paper" && computer === "scissors"
+            ?`playerOne: ${player}\nComputer: ${computer}\nComputer wins!`
+            :player === "scissors" && computer === "rock"
+            ? `playerOne: ${player}\nComputer: ${computer}\nComputer Wins!`
+            :`playerOne: ${player}\nComputer: ${computer}\nplayerOne wins!`
+
+    return winner
+};
+
+const displayResult = (result) => {
+    alert(result);
+};
+
+const askToPlayAgain = () => {
+    return confirm("play Again?");
+};
+
+const thanksForPlaying = () => {
+    alert("Ok, thanks for playing ");
+};
+
+initGame();
+
